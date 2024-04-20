d06790f32bd51ac1a67db95e0d201c1b590921cd
Milos Bielik <milos.bielik@gmail.com>
2024-04-20 12:34:06 +0200

    initial commit of my first test maven  project

---
+++ src/main/java/com/kyberos/mvntest/Main.java
@@ -0,0 +1,34 @@
+package com.kyberos.mvntest;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfName;
+import com.lowagie.text.pdf.PdfString;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // step 1: creation of a document-object
+        
+        try (Document document = new Document()){
+            // step 2:
+            // we create a writer that listens to the document
+            // and directs a PDF-stream to a file
+            final PdfWriter instance = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+
+            // step 3: we open the document
+            document.open();
+            instance.getInfo().put(PdfName.CREATOR, new PdfString(Document.getVersion()));
+            // step 4: we add a paragraph to the document
+            document.add(new Paragraph("Hello World"));
+        } catch (DocumentException | IOException de) {
+            System.err.println(de.getMessage());
+        }
+    }
+}
